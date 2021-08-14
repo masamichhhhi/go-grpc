16d9ac5a86f9b8a9e416488d5321d8228b2eff81
masamichhhhi <masamichhhi@gmail.com>
2021-08-14 10:47:15 +0900

    First commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module grpc-sample
+
+go 1.13
+
+require (
+	github.com/pkg/errors v0.9.1
+	google.golang.org/grpc v1.40.0
+	google.golang.org/protobuf v1.25.0
+)
---
+++ server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/pkg/errors"
+	"google.golang.org/grpc"
+
+	pb "grpc-sample/pb/calc"
+)
+
+const port = ":50051"
+
+type ServerUnary struct {
+	pb.UnimplementedCalcServer
+}
+
+func (s *ServerUnary) Sum(ctx context.Context, in *pb.SumRequest) (*pb.SumReply, error) {
+	a := in.GetA()
+	b := in.GetB()
+	fmt.Println(a, b)
+	reply := fmt.Sprintf("%d + %d = %d", a, b, a+b)
+	return &pb.SumReply{
+		Message: reply,
+	}, nil
+}
+
+func set() error {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		return errors.Wrap(err, "ポート失敗")
+	}
+	s := grpc.NewServer()
+	var server ServerUnary
+	pb.RegisterCalcServer(s, &server)
+	if err := s.Serve(lis); err != nil {
+		return errors.Wrap(err, "サーバー起動失敗")
+	}
+
+	return nil
+}
+
+func main() {
+	fmt.Println("start")
+	if err := set(); err != nil {
+		log.Fatal("%v", err)
+	}
+}
